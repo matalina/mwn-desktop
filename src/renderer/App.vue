6d41047e7b8fb530e585e78440b9d3a1cbcc53bd
Alicia Wilkerson <aliciawilkerson@babers.com>
2019-02-01 14:22:28 -0600

    working vue electron app

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'my-writing-notebook'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
