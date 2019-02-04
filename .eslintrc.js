952538574ffa8b2b2f927249927ae86289818096
Alicia Wilkerson <aliciawilkerson@babers.com>
2019-02-04 10:04:24 -0600

    no menu, minimize close to tray, open/quit tray menu

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,11 +16,15 @@ module.exports = {
     'html'
   ],
   'rules': {
+      //"semi": ["error","always"],
+      //"indent": ["error", 4],
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+
+
   }
 }
--- src/main/index.js
+++ src/main/index.js
@@ -1,13 +1,16 @@
 'use strict'
 
-import { app, BrowserWindow } from 'electron'
+import electron from 'electron'
+let path = require('path')
 
+const app = electron.app
+let isQuiting = false
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
@@ -15,20 +18,73 @@ const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
 
+let icon = './build/icons/icon.ico'
+
 function createWindow () {
   /**
    * Initial window options
    */
-  mainWindow = new BrowserWindow({
+  mainWindow = new electron.BrowserWindow({
     height: 563,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    icon
   })
 
   mainWindow.loadURL(winURL)
+  /*
+  const menuTemplate = [
+    {
+      label: 'Electron',
+      submenu: [
+
+      ]
+    }
+  ]
+  const menu = electron.Menu.buildFromTemplate(menuTemplate)
+  electron.Menu.setApplicationMenu(menu) */
+
+  mainWindow.setMenuBarVisibility(false)
+
+  let tray = new electron.Tray(icon)
+  let template = [{
+    label: 'Open',
+    click () {
+      mainWindow.show()
+    }
+  },
+  {
+    label: 'Quit',
+    click () {
+      isQuiting = true
+      app.quit()
+    }
+  }]
+
+  let context = electron.Menu.buildFromTemplate(template)
+
+  tray.setContextMenu(context)
+  tray.setToolTip('My Writing Notebook')
+
+  mainWindow.on('minimize', (event) => {
+    event.preventDefault()
+    mainWindow.minimize()
+  })
+
+  mainWindow.on('show', () => {
+    tray.setHighlightMode('always')
+  })
 
   mainWindow.on('closed', () => {
     mainWindow = null
+  })
+
+  mainWindow.on('close', (event) => {
+    if (!isQuiting) {
+      event.preventDefault()
+      mainWindow.minimize()
+      event.returnValue = false
+    }
   })
 }
 
