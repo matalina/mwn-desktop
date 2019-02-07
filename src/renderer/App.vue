3f5bd4496215ac48429a651c7cfc0ef21d04a8df
matalina <matalina@gmail.com>
2019-02-07 08:32:32 -0600

    added bootstrap

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -10,5 +10,16 @@
   }
 </script>
 
-<style>
+<style lang="scss">
+  /* https://coolors.co/122d29-ec5800-fff9f7-385652-2d2d2d */
+  $primary: #122D29;
+  $secondary: #EC5800;
+  $info: #385652;
+  $light: #FFF9F7;
+  $dark: #2D2D2D;
+
+  $body-bg: $light;
+  $body-color: $dark;
+  
+  @import "../../node_modules/bootstrap/scss/bootstrap";
 </style>
